4d9fafd8e806cdf2e450883ed6238fa6d41815c2
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-05-04 21:27:50 +0900

    update(front): ページリロード時の処理を修正

--- front/plugins/firebase.authentication.js
+++ front/plugins/firebase.authentication.js
@@ -1,18 +1,24 @@
-// https://www.metrocode.co/blog/post/firebase-authentication-nuxt-vuejs#:~:text=%E4%BD%9C%E6%88%90%E3%81%97%E3%81%BE%E3%81%99%E3%80%82-,onAuthStateChanged,-%E3%81%A8%E3%81%84%E3%81%86%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%81%AF
+// リロード時のリダイレクト処理など
 // https://zenn.dev/phi/articles/firebase-auth-wait-for-initialization
-
-export default (context) => {
-  // console.log('context:', context)
-  const { $fire, store } = context
-  // console.log(route.name)
-
+// https://tns-blog.com/275 を参考にするとログイン画面に戻ってしまう
+// 非同期処理をしていないことでauthenticated.jsが先に実行されることが原因と思われる
+// https://www.metrocode.co/blog/post/firebase-authentication-nuxt-vuejs#:~:text=%E4%BD%9C%E6%88%90%E3%81%97%E3%81%BE%E3%81%99%E3%80%82-,onAuthStateChanged,-%E3%81%A8%E3%81%84%E3%81%86%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%81%AF を参考にして修正
+export default function ({ $axios, $fire, redirect, route, store }) {
   return new Promise((resolve, reject) => {
     $fire.onAuthStateChanged($fire.auth, (user) => {
       // // https://firebase.google.com/docs/reference/js/firebase.User
       if (user) {
-        console.log('user: ', user)
+        const token = user.accessToken
+        $axios.setToken(token, 'Bearer')
+
         const { email, uid } = user
         store.dispatch('auth/setUser', { email, uid })
+      } else {
+        $axios.setToken(null, 'Bearer')
+
+        if (route.path !== '/login' && route.path !== '/signup') {
+          redirect('/login')
+        }
       }
       resolve()
     })
