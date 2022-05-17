a2de9fd1eb0fd38f9cecfa2dc36b738455d19683
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-05-17 21:40:43 +0900

    add(front): 初回ユーザー登録用ページのレイアウトファイルを追加

---
+++ front/layouts/welcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-main>
+      <nuxt />
+      <WelcomeAppFooter />
+    </v-main>
+  </v-app>
+</template>
+<script>
+import WelcomeAppFooter from '@/components/Welcome/WelcomeAppFooter'
+export default {
+  components: {
+    WelcomeAppFooter,
+  },
+}
+</script>
+<style>
+.height-fullfilled {
+  height: 100vh;
+}
+
+.width-fullfilled {
+  width: 100%;
+}
+
+.img-woman {
+  max-width: 100%;
+  height: auto;
+}
+</style>
