43d2229afd5915f0476691c1f6c22522d8b4c360
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-05-18 17:09:05 +0900

    feat(front): 新規ユーザー用のトップページをcomponentに差し替え

---
+++ front/components/Welcome/WelcomeAbout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="height-fullfilled d-flex justify-center align-center">
+    <div>
+      <v-row class="height-fullfilled" justify="center" align-content="center">
+        <v-col cols="12" align="center">
+          <!-- TODO: h1タグのグラフィティを変更する -->
+          <h1 class="text-h2">Medcool</h1>
+        </v-col>
+        <v-col cols="12" align="center">
+          <h3 class="mt-4 text-h5">
+            <p>みんなで瞑想するアプリです!!</p>
+            <p>瞑想を習慣化して</p>
+            <p>毎日 Happy、Powerfulにすごそう!!</p>
+          </h3>
+          <img
+            src="@/assets/image/woman_yoga_meisou.webp"
+            class="img-responsive"
+          />
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -9,41 +9,18 @@
   </div> -->
   <!-- <div>index.vue</div> -->
   <div>
-    <v-container class="height-fullfilled width-fullfilled">
-      <v-row class="height-fullfilled" justify="center" align-content="center">
-        <v-col align="center">
-          <!-- TODO: h1タグのグラフィティを変更する -->
-          <h1 class="text-h2">Medcool</h1>
-          <h3 class="mt-4 text-h5">
-            瞑想を習慣化して<br />毎日 Happy、Powerfulに!!
-          </h3>
-          <img src="@/assets/image/woman_yoga_meisou.webp" class="img-woman" />
-          <p>
-            <v-btn color="blue lighten-2" block x-large
-              ><span class="white--text text-h6 font-weight-bold"
-                >はじめる</span
-              >
-            </v-btn>
-          </p>
-          <p>
-            <v-btn block x-large
-              ><span class="text-h6 font-weight-regular">なぜ瞑想？</span>
-            </v-btn>
-          </p>
-
-          <p>
-            <v-btn block x-large>
-              <span class="text-h6 font-weight-regular">使い方</span>
-            </v-btn>
-          </p>
-        </v-col>
-      </v-row>
+    <v-container>
+      <WelcomeAbout />
+      <WelcomeWhyMeditation />
     </v-container>
   </div>
   <!-- TODO: ログイン済みユーザーのページ情報を追加する -->
 </template>
 
 <script>
+import WelcomeAbout from '@/components/Welcome/WelcomeAbout'
+import WelcomeWhyMeditation from '@/components/Welcome/WelcomeWhyMeditation'
+
 export default {
   // [Nuxt - Layouts directory](https://nuxtjs.org/docs/directory-structure/layouts/#custom-layout)
   // layout({ store }) {
@@ -51,7 +28,10 @@ export default {
   // },
 
   name: 'PagesIndex',
-  components: {},
+  components: {
+    WelcomeAbout,
+    WelcomeWhyMeditation,
+  },
   layout: 'welcome',
 }
 </script>
